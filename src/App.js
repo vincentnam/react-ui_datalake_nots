27942934afe2f0ca01ac4f1a73e96ab69eeace93
vincentnam <dang.vincentnam@gmail.com>
2020-09-16 18:57:10 +0200

    Dropzone card done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Grid from '@material-ui/core/Grid';
+import DropZoneCard from './components/DropZoneCard'
 function App() {
+
+
+
+  const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  var componentConfig = { postUrl: 'no-url' };
+  var djsConfig = { autoProcessQueue: true, dictDefaultMessage: "Déposez un fichier à téléverser - Drop file to upload it", addRemoveLinks:true, multiple: true }
+  var eventHandlers = { addedfile: (file) => console.log(file) }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+
+        </Grid>
+        <Grid item xs={6} sm={3}>
+
+        <DropZoneCard config={componentConfig}
+                      eventHandlers={eventHandlers}
+                      djsConfig={djsConfig}></DropZoneCard>
+          {/*<Paper className={classes.paper}>xs=12</Paper>*/}
+
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
---
+++ src/components/DropZoneCard.js
@@ -0,0 +1,130 @@
+import React, {createRef, useCallback, useState} from "react";
+
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from '@material-ui/core/styles';
+import {ListGroup} from "react-bootstrap";
+import Dropzone, {useDropzone} from "react-dropzone";
+import {View} from "react-native-web";
+import axios, { post } from 'axios';
+
+
+const api_rest = "http://127.0.0.1:5000"
+const upload_file_url="/upload_file"
+
+export default function DropZoneCard (props){
+    function upload_file(file){
+        const formData = new FormData();
+        formData.append('file',file);
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        }
+        return post(api_rest + upload_file_url, formData, config)
+
+    }
+    function send_click(e){
+
+
+        files.forEach(file => console.log(file))
+        files.forEach(file => upload_file(file).then(response => console.log(response)))
+    }
+    const [files, setFiles] = useState([]);
+    const dropzoneRef = createRef();
+    const openDialog = () => {
+        // Note that the ref is set async,
+        // so it might be null at some point
+        if (dropzoneRef.current) {
+            dropzoneRef.current.open()
+        }
+    };
+
+    const onDrop = useCallback((acceptedFiles) => {
+        acceptedFiles.forEach((file) => {
+            files.push(file)
+            setFiles(files)
+        })
+
+    }, [])
+    const {getRootProps, getInputProps} = useDropzone({onDrop})
+
+    return <Card>
+        {/*<DropzoneComponent ref="dropzone" config={props.config}*/}
+        {/*                   eventHandlers={props.eventHandlers}*/}
+        {/*                   djsConfig={props.djsConfig}*/}
+        {/*                   onChange={setdropFiles}*/}
+        {/*/>*/}
+
+
+        {/*<Dropzone onDrop={acceptedFiles => console.log(acceptedFiles)}>*/}
+        {/*    {({getRootProps, getInputProps}) => (*/}
+        {/*        <section>*/}
+        {/*            <div {...getRootProps()}>*/}
+        {/*                <input {...getInputProps()} />*/}
+        {/*                <p>Drag 'n' drop some files here, or click to select files</p>*/}
+        {/*            </div>*/}
+        {/*        </section>*/}
+        {/*    )}*/}
+        {/*</Dropzone>*/}
+        <Dropzone  ref={dropzoneRef} onDrop={onDrop} noClick noKeyboard addRemoveLink={true} on>
+            {({getRootProps, getInputProps, acceptedFiles}) => {
+                return (
+                    <div className="container">
+                        <View style={{ textAlign:'center'}}>
+                            <div  {...getRootProps({className: 'dropzone'})} >
+                                <input {...getInputProps()} />
+                                <p >Glisser-déposer un fichier dans cette zone</p>
+                                <button
+                                    type="button"
+                                    onClick={openDialog}
+                                >
+                                    Choisir un fichier
+                                </button>
+                            </div>
+                        </View>
+                        <View style={{ textAlign:'center'}}>
+                            <aside>
+                                <h4>Files</h4>
+                                <ul>
+                                    {files.map(file => (
+                                        <li key={file.path}>
+                                            {file.path} - {file.size} bytes
+                                        </li>
+                                    ))}
+                                </ul>
+                            </aside>
+                        </View>
+                    </div>
+                );
+            }}
+        </Dropzone>
+        <View >
+            <Button onClick={send_click} variant="contained" color="primary">
+                Téleverser - Upload
+            </Button>
+        </View>
+    </Card>
+}
+//
+// export class DropZoneCard extends React.Component {
+//
+//
+//     getDropZoneHandler(event){
+//
+//     }
+//     constructor (props) {
+//         super(props)
+//
+//         this.state = { files: [] }
+//     }
+//     render() {
+//
+//
+//
+//     }
+//
+// }
+
+// export default DropZoneCard
