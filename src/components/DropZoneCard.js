5b8f1e1ce3dad62c80bef5f1ac5cfa42c2095cb8
Vincent-Nam DANG <vdang@minizup.irit.fr>
2020-10-05 15:34:04 +0200

    fix import material-ui

--- src/components/DropZoneCard.js
+++ src/components/DropZoneCard.js
@@ -1,8 +1,7 @@
 import React, {createRef, useCallback, useState} from "react";
 
-import Button from "@material-ui/core/Button";
-import Card from "@material-ui/core/Card";
-import Grid from "@material-ui/core/Grid";
+import {Button, Card, Grid} from "@material-ui/core";
+
 import { makeStyles } from '@material-ui/core/styles';
 import {ListGroup} from "react-bootstrap";
 import Dropzone, {useDropzone} from "react-dropzone";
